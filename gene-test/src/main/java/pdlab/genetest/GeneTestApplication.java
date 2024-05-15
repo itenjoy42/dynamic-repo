be450bf26670d40183f678e2c3bfb828b29361ab
itenjoy42 <itenjoy42@gmail.com>
2024-05-15 15:18:07 +0900

    1.0

---
+++ gene-test/src/main/java/pdlab/genetest/GeneTestApplication.java
@@ -0,0 +1,13 @@
+package pdlab.genetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneTestApplication.class, args);
+	}
+
+}
---
+++ gene-test/src/test/java/pdlab/genetest/GeneTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pdlab.genetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
